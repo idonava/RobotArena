6f0b35c11500e10a7b69b4186d8b2a024ec90cb1
or <or.bngigi@gmail.com>
2016-05-22 18:14:55 +0300

    battery added

--- Battery.py
+++ Battery.py
@@ -1,6 +1,11 @@
 
-
 class Battery:
 
-    def __init__(self, list):
-        self.list=list
+    def __init__(self):
+        self.bat=float(100)
+
+    def charge(self):
+        self.bat=min(100,self.bat + 2.4)
+
+    def unCharge(self):
+        self.bat = max(0, self.bat - 2.4)
--- Robot.py
+++ Robot.py
@@ -1,6 +1,7 @@
 import random
 import threading
 import time
+import Battery
 
 class Robot(threading.Thread):
 
@@ -11,16 +12,23 @@ class Robot(threading.Thread):
         self.X=X
         self.Y=Y
         self.color=color
+        self.Battery=Battery.Battery()
 
     def run(self):
         while(True):
             self.move()
-            time.sleep(0.1)
+            self.update_bat()
+            time.sleep(1)
 
     def move(self):
-        return int(random.random()*4)
+        if(self.Battery.bat>0):
+            return int(random.random()*4)
 
-
+    def update_bat(self):
+        if(self.color==0):
+            self.Battery.charge()
+        else:
+            self.Battery.unCharge()
 
 
 
