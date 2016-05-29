8449cf3192832cf3bb39c5f9a755022791fb51ea
Rony <rblejter@gmail.com>
2016-05-29 21:05:11 +0300

    added some details to the pop up

--- Arena.py
+++ Arena.py
@@ -64,8 +64,12 @@ class Arena:
             if rob[0].id == tags:
                 robot = rob
                 break
-        string="Actual Position: "+str(robot[1])+" "+str(robot[2])+"\n\nGuess position: "+str(robot[0].X)+" "+str(robot[0].Y) +"\n\nBatery: "+str(robot[0].Battery.bat)+ "%"
-        tkMessageBox.showinfo("Robot information",string )
+        if robot[0].isStatic:
+            string = "Actual Position: " + str(robot[1]) + " " + str(robot[2])
+            tkMessageBox.showinfo("Static Robot ID: "+ str(tags), string)
+        else:
+            string="Actual Position: "+str(robot[1])+" "+str(robot[2])+"\n\nGuess position: "+str(robot[0].X)+" "+str(robot[0].Y) +"\n\nBatery: "+str(robot[0].Battery.bat)+ "%"
+            tkMessageBox.showinfo("Moving Robot ID: "+str(tags),string )
 
         print(event.widget.find_closest(event.x, event.y))
 
